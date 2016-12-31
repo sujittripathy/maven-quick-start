aaf3ed2c1bd7f93e842b6ae09a3874d4ea7febd2
sujittripathy <sujit.tripathy@icloud.com>
2016-12-31 00:21:39 -0800

    Changed for version 1.8 compiler

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,20 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    
+    List<String> strList;
     public Application() {
         System.out.println ("Inside Application");
+		strList = new ArrayList<>();
+		strList.add("Hello");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	System.out.println(strList);
     }
 }
