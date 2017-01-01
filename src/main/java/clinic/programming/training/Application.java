4e5e736ababd8d070dbe84f3ffd669a96aa98a0b
sujittripathy <sujit.tripathy@icloud.com>
2016-12-31 17:02:32 -0800

    use StringUtil and added dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,20 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     List<String> strList;
+	
+	public int countWords(String words){
+		String[] wordsSplit = StringUtils.split(words, ' ');
+		return (wordsSplit == null)? 0 : wordsSplit.length;
+	}
+	
+	public void greet(){
+		System.out.println ("Inside greet..");
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
 		strList = new ArrayList<>();
@@ -14,7 +25,11 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
-	System.out.println(strList);
+		Application app = new Application();
+		app.greet();
+		int count = app.countWords("Hello There");
+		System.out.println("Application words : "+count);
     }
+	
+	
 }
